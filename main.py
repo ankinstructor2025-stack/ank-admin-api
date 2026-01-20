098bc82c4bb34b5945c3f3a230cf6bfa042d9598
ankinstructor2025-stack <ank.instructor2025@gmail.com>
2026-01-20 21:09:38 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import os
+import psycopg2
 
 app = FastAPI()
 
@@ -9,3 +11,31 @@ def health():
 @app.get("/v1/session")
 def get_session():
     return {"state": "OK"}
+
+def get_db():
+    instance = os.environ["INSTANCE_CONNECTION_NAME"]  # project:region:instance
+    dbname = os.environ["DB_NAME"]
+    user = os.environ["DB_USER"]
+    password = os.environ["DB_PASSWORD"]
+
+    conn = psycopg2.connect(
+        host=f"/cloudsql/{instance}",
+        dbname=dbname,
+        user=user,
+        password=password,
+    )
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+@app.get("/v1/debug/users-select")
+def users_select(conn=Depends(get_db)):
+    with conn.cursor() as cur:
+        cur.execute("SELECT user_id FROM users;")
+        rows = cur.fetchall()
+
+    return {
+        "rows": rows,
+        "row_count": len(rows),
+    }
